7b39fb57ba096c49ddb5b7acd6dec3901e415d6c
ArshadMalikx <125849950+ArshadMalikx@users.noreply.github.com>
2025-05-17 12:45:00 +0530

    Add files via upload

---
+++ Ip final/Ip final/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WebcamCapture from './components/WebcamCapture';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Emotion Detector</h2>
+      <WebcamCapture />
+    </div>
+  );
+}
+
+export default App;
